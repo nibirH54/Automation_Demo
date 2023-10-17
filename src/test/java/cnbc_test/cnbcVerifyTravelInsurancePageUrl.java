2e3517b45390d9dbc1a94655be3697794ca75dd3
nibirH54 <nibirw@gmail.com>
2023-10-17 19:38:35 -0400

    10/17/2023 - Today's second commit

---
+++ src/test/java/cnbc_test/cnbcVerifyTravelInsurancePageUrl.java
@@ -0,0 +1,31 @@
+package cnbc_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class cnbcVerifyTravelInsurancePageUrl {
+
+    WebDriver driver = new FirefoxDriver();
+
+    @Test
+    public void verifyTravelInsurancePageUrl() throws InterruptedException {
+        driver.get("https://www.cnbc.com/");
+        driver.manage().window().maximize();
+        WebElement element = driver.findElement(By.xpath("//button[@class='EditionMenu-featuredMenuButton']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).build().perform();
+        driver.findElement(By.xpath("//p[normalize-space()='Insurance']")).click();
+        driver.findElement(By.xpath("//p[normalize-space()='Travel Insurance']")).click();
+        Thread.sleep(4000);
+        String travelInsurancePageUrl = driver.getCurrentUrl();
+        System.out.println("The travel insurance Page Url is: " + travelInsurancePageUrl);
+        Thread.sleep(3000);
+        String articleHeader = driver.findElement(By.xpath("//h1[@class='ArticleHeader-styles-select-headline--n2eyV']")).getText();
+        System.out.println("The Article Header Text is: " + articleHeader);
+        driver.close();
+    }
+}
